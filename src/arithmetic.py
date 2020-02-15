7b63796ff33230395d3725b4fb27b07cb02c308c
prkapur <pranavkapur91@gmail.com>
2020-02-15 23:08:21 +0800

    Adding repository

---
+++ src/arithmetic.py
@@ -0,0 +1,38 @@
+import logging
+
+class Arithmetic():
+    """ A Class for simple math operations """
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def addition(self):
+        """ Adding two variables """
+        self.add = self.a + self.b
+        return self.add
+
+    def subtraction(self):
+        """ Subtracting two variables """
+        self.sub = self.a - self.b
+        return self.sub
+    
+    def multiplication(self):
+        """ Multiplying two variables """
+        self.mul = self.a * self.b
+        return self.mul
+
+    def division(self):
+        """ Dividing two variables """
+        try:
+            self.div = self.a / self.b
+            return self.div
+        except ZeroDivisionError:
+            pass
+        
+
+if __name__ == "__main__":
+    arith = Arithmetic(3,5)
+    print(f"Add:{arith.addition()}")
+    print(f"Sub:{arith.subtraction()}")
+    print(f"Mul:{arith.multiplication()}")
+    print(f"Div:{arith.division()}")
---
+++ tests/test_operations.py
@@ -0,0 +1,26 @@
+# python -m unittest tests.test_operations
+import unittest
+from src.arithmetic import Arithmetic
+
+class TestArithmeticOperations(unittest.TestCase):
+
+    def setUp(self):
+        self.arith = Arithmetic(6,4)
+
+    def test_addition(self):
+        self.assertEqual(self.arith.addition(),10)
+
+    def test_subtraction(self):
+        self.assertEqual(self.arith.subtraction(),2)
+
+    def test_multiplication(self):
+        self.assertEqual(self.arith.multiplication(),24)
+
+    def test_division(self):
+        self.assertEqual(self.arith.division(),2.5)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+import unittest
